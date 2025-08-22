8b1b1312d4c2a7297fc0cb490ba0acb56d1fd9fe
CRISScoD19 <cristiansyl098@gmail.com>
2025-08-22 14:31:52 -0500

    hola

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        AsistenciaApplication.main(args);
+    }
+}
